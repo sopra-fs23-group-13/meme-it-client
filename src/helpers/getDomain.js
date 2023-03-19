1540d094e75b42623488be58a231a74d44566487
Marc <90235428+HansiHusten@users.noreply.github.com>
2023-03-19 17:34:06 +0100

    Update getDomain.js

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -3,7 +3,7 @@ import { isProduction } from 'helpers/isProduction'
 /**
  * This helper function returns the current domain of the API.
  * If the environment is production, the production App Engine URL will be returned.
- * Otherwise, the link localhost:8080 will be returned (Spring server default port).
+ * Otherwise, the link localhost:8080 will be returned (Spring server default port)
  * @returns {string}
  */
 export const getDomain = () => {
