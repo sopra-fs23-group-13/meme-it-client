ff3b4e38eb5fafdd71e32684411f4f00fe2de1af
dani <lutzidan@students.zhaw.ch>
2023-03-29 14:41:20 -0500

    added icon to lobbylist

--- src/components/views/LobbyList.js
+++ src/components/views/LobbyList.js
@@ -1,33 +1,57 @@
 import React from "react";
 import {ListGroup, Button, Badge} from "react-bootstrap";
+import "styles/views/LobbyList.scss";
+import { TfiReload } from 'react-icons/tfi';
+
 
 import MockData from '../../mockData/menuScreenDataMock.json'
 
 const LobbyList = () => {
 
     return (
-        <ListGroup as="ol">
-            {MockData.map(lobby => {
-                return(
-                    <ListGroup.Item
-                        as="li"
-                        className="d-flex justify-content-between align-items-start"
-                    >
-                        <div className="ms-2 me-auto">
-                            <div className="fw-bold">{lobby.name}</div>
-                            Admin: {lobby.admin}
-                        </div>
-                        <div>
-                            <Badge className={"align-self-center home playerSize"} bg={lobby.size === lobby.current_players ? "danger" : "primary"} pill>
-                                {lobby.current_players} / {lobby.size}
-                            </Badge>
-                        </div>
-                        <Button>
-                            Join Lobby
-                        </Button>
-                    </ListGroup.Item>
-                )
-            })}
-        </ListGroup>
+        <div>
+            <ListGroup as="ol">
+            <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+            >
+                <div className="ms-2 me-auto">
+                    <div className="fw-bold">Game name</div>
+
+                </div>
+                <div>
+                    <Badge className={"align-self-center lobbyList playerSize"} pill>
+                        Joined / Players
+                    </Badge>
+                </div>
+                <Button variant={"warning"}>
+                    <TfiReload />
+                </Button>
+            </ListGroup.Item>
+            </ListGroup>
+            <ListGroup as="ol">
+                {MockData.map(lobby => {
+                    return(
+                        <ListGroup.Item
+                            as="li"
+                            className="d-flex justify-content-between align-items-start"
+                        >
+                            <div className="ms-2 me-auto">
+                                <div className="fw-bold">{lobby.name}</div>
+                                Admin: {lobby.admin}
+                            </div>
+                            <div>
+                                <Badge className={"align-self-center lobbyList playerSize"} bg={lobby.size === lobby.current_players ? "danger" : "primary"} pill>
+                                    {lobby.current_players} / {lobby.size}
+                                </Badge>
+                            </div>
+                            <Button>
+                                Join
+                            </Button>
+                        </ListGroup.Item>
+                    )
+                })}
+            </ListGroup>
+        </div>
     )
 }; export default LobbyList;
