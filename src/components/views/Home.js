ecd1788eb252c8ad4f8ab9f666ead82bd6e15b35
daniellutziger <daniel.lutziger@uzh.ch>
2023-04-23 23:02:27 +0200

    added new background to the game, animated and stuff. looks cool i guess

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -6,6 +6,7 @@ import {FormField} from "../../helpers/formField";
 import UsernameModal from "./UsernameModal";
 import {useHistory} from "react-router-dom";
 import {Notification} from "../ui/Notification";
+import {AnimatedBackground} from "styles/images/AnimatedBackground"
 
 
 const Home = () => {
@@ -16,12 +17,12 @@ const Home = () => {
 
     const toggleShowAlert = () => setShowAlert(!showAlert);
 
-    const lobbyList  = () => {
-        return(
+    const lobbyList = () => {
+        return (
             <Container>
                 <Row>
                     <Col>
-                        <LobbyList action={joinExistingGame} />
+                        <LobbyList action={joinExistingGame}/>
                     </Col>
                 </Row>
             </Container>
@@ -44,70 +45,84 @@ const Home = () => {
         history.push("/lobby");
     }
     return (
-        <Container className={"home content"}>
-            <div className={"home alert"}>
-            <Notification reason={sessionStorage.getItem("alert")}
-                          showAlert={showAlert}
-                          toggleShowAlert={toggleShowAlert}
-            />
-            </div>
-            <Stack gap={3}>
-            <Row>
-                <Container>
-                    <Row>
-                        <Col sm>{/*placeholder*/}</Col>
-                        <Col sm>
-                            <Stack gap={3}>
-                            <Row>
-                                <Col>
-                                    <h1 className="home title">Meme-It</h1>
-                                    <p className="home subtitle"> The Meme Creation Game</p>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col>
-                                    <FormField
-                                        placeholder="Game Code"
-                                        code={gameCode}
-                                        name="code"
-                                        label="Game Code"
-                                        action={joinExistingGame}
-                                        onChange={handleChange}
-                                        c_names="home join-btn"
-                                    />
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col>
-                                    <UsernameModal c_names={"home buttons"} title={"Create Lobby"} submit={createNewGame} />
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col>
-                                    <Button className={"home buttons"} onClick={() => setShow(!show)} >{show ? "Close List" : "Show Games"}</Button>
-                                </Col>
-                            </Row>
-                            </Stack>
-                        </Col>
-                        <Col sm>{/*placeholder*/}</Col>
-                    </Row>
-                </Container>
-            </Row>
-            <Row>
-                <Col>
-                    {show && lobbyList()}
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <p className = "home tutorial-title">How To Play</p>
-                    <div className ="home tutorial-text">
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        <div className={"home overall"}>
+            <AnimatedBackground/>
+            <Container className={"home content"}>
+                <div className={"home card"}>
+                    <div className={"home alert"}>
+                        <Notification reason={sessionStorage.getItem("alert")}
+                                      showAlert={showAlert}
+                                      toggleShowAlert={toggleShowAlert}
+                        />
                     </div>
-                </Col>
-            </Row>
-            </Stack>
-        </Container>
+                    <Stack gap={3}>
+                        <Row>
+                            <Container>
+                                <Row>
+                                    <Col sm>{/*placeholder*/}</Col>
+                                    <Col sm>
+                                        <Stack gap={3}>
+                                            <Row>
+                                                <Col>
+                                                    <h1 className="home title">Meme-It</h1>
+                                                    <p className="home subtitle"> The Meme Creation Game</p>
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col>
+                                                    <FormField
+                                                        placeholder="Game Code"
+                                                        code={gameCode}
+                                                        name="code"
+                                                        label="Game Code"
+                                                        action={joinExistingGame}
+                                                        onChange={handleChange}
+                                                        c_names="home join-btn"
+                                                    />
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col>
+                                                    <UsernameModal c_names={"home buttons"} title={"Create Lobby"}
+                                                                   submit={createNewGame}/>
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col>
+                                                    <Button className={"home buttons"}
+                                                            onClick={() => setShow(!show)}>{show ? "Close List" : "Show Games"}</Button>
+                                                </Col>
+                                            </Row>
+                                        </Stack>
+                                    </Col>
+                                    <Col sm>{/*placeholder*/}</Col>
+                                </Row>
+                            </Container>
+                        </Row>
+                        <Row>
+                            <Col>
+                                {show && lobbyList()}
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <p className="home tutorial-title">How To Play</p>
+                                <div className="home tutorial-text">
+                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                                    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+                                    eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+                                    takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+                                    consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+                                    dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+                                    dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+                                    ipsum dolor sit amet.
+                                </div>
+                            </Col>
+                        </Row>
+                    </Stack>
+                </div>
+            </Container>
+        </div>
     )
 }
 
