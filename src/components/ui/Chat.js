ca5cf7e770e5ddd642dfc89be0e3f20342ba961e
daniellutziger <daniel.lutziger@uzh.ch>
2023-05-07 11:38:48 +0200

    styling fixed closes #89

--- src/components/ui/Chat.js
+++ src/components/ui/Chat.js
@@ -1,10 +1,12 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import ChatRoom from "./ChatRoom";
 import "styles/ui/Chat.scss";
 import Cookies from "universal-cookie";
 import PropTypes from "prop-types";
 import {api} from "../../helpers/api";
 import {chat} from "../../helpers/endpoints";
+import {Badge} from "react-bootstrap";
+
 const Chat = (props) => {
     Chat.propTypes = {
         currentLobby: PropTypes.object,
@@ -22,10 +24,8 @@ const Chat = (props) => {
         setCollapsed(!collapsed);
     };
     const newMessageText = () => {
-        if(sessionStorage.getItem("newMessages") == null || sessionStorage.getItem("newMessages") == 0) return ("Open Chat");
-        else {
-            return (`Open Chat (${sessionStorage.getItem("newMessages")})`)
-        }
+        if(sessionStorage.getItem("newMessages") > 0) return (<><span style={{ textAlign: 'center', width: '100%', marginLeft: '22px'}}>Open Chat</span> <Badge pill bg="danger" style={{ marginLeft: 'auto' }}>{sessionStorage.getItem("newMessages")}</Badge></>);
+        return ("Open Chat");
     }
     if(sessionStorage.getItem("lastOpenedAt") === null) sessionStorage.setItem("lastOpenedAt", 0);
     if(sessionStorage.getItem("newMessages") === null) sessionStorage.setItem("newMessages", 0);
@@ -53,12 +53,17 @@ const Chat = (props) => {
             </button>
             {!collapsed && (
                 <div className="chat-room-container">
+
+                    <div className="chat-room-wrapper">
+                        <div className="chat-room-content">
                     <ChatRoom
                         name={props.currentLobby?.owner?.name || props.currentLobby.id}
                         code={localStorage.getItem("code")}
                         uuid={props.currentLobby}
                         author={props.currentLobby.players.find((player) => player?.id === cookies.get("token") || player?.user?.id === cookies.get("token"))}
                     />
+                        </div>
+                    </div>
                 </div>
             )}
         </div>
